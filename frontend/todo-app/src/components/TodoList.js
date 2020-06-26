e2b72812fb457423bc872ba5e0413d35fbcd3e27
HansaniTharaka <66997469+HansaniTharaka@users.noreply.github.com>
2020-06-26 17:54:39 +0530

    add a key to the items of the list

--- frontend/todo-app/src/components/TodoList.js
+++ frontend/todo-app/src/components/TodoList.js
@@ -32,7 +32,7 @@ export default class ListTodosComponent extends Component {
                         {
                             this.state.todos.map (
                                 todo =>
-                                    <tr>
+                                    <tr key={todo.id}>
                                         <td>{todo.description}</td>
                                         <td>{todo.done.toString()}</td>
                                         <td>{todo.targetDate.toString()}</td>
