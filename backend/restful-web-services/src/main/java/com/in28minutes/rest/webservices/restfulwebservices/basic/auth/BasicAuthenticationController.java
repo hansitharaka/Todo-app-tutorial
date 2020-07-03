f7c2306f03c58a72d9bf0581cad91f795b76ccaa
HansaniTharaka <66997469+HansaniTharaka@users.noreply.github.com>
2020-07-04 03:11:31 +0530

    create basic Authenticatio restful service in springboot

--- backend/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/auth/BasicAuthenticationController.java
+++ backend/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/auth/BasicAuthenticationController.java
@@ -2,7 +2,6 @@ package com.in28minutes.rest.webservices.restfulwebservices.basic.auth;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
