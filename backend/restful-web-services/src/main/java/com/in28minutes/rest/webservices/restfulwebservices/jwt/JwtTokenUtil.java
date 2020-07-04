3df9e3ec97903eb56d669ad514a0bc3157edb3c8
HansaniTharaka <66997469+HansaniTharaka@users.noreply.github.com>
2020-07-04 20:20:50 +0530

    made everything error free. Had a problem in pom.xml dependency

--- backend/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/jwt/JwtTokenUtil.java
+++ backend/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/jwt/JwtTokenUtil.java
@@ -1,7 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices.jwt;
 
 import java.io.Serializable;
-import java.time.Clock;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,7 +10,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import com.sun.org.apache.xml.internal.security.algorithms.SignatureAlgorithm;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Clock;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClock;
 
 @Component
 public class JwtTokenUtil implements Serializable {
